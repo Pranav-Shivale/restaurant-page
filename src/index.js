702b73043f31e9de7f4d540b0c19873441d80129
Pranav-Shivale <pranavshivale69@gmail.com>
2026-05-25 22:25:15 +0530

    Import and call home and menu modules

--- src/index.js
+++ src/index.js
@@ -1,2 +1,16 @@
 import "./styles.css"
-console.log("Webpage running!");
+import loadHome from "./home.js";
+import loadMenu from "./menu.js";
+loadHome();
+
+const homeBtn = document.querySelector("#home-btn");
+const menuBtn = document.querySelector("#menu-btn");
+const aboutBtn = document.querySelector("#about-btn");
+
+homeBtn.addEventListener("click", () => {
+  loadHome();
+})
+
+menuBtn.addEventListener("click", () => {
+  loadMenu();
+})
